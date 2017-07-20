d46921db20fb87344f37b1ffd071f79bb8d5a51e
Unknown <cutlerywalab@gmail.com>
2017-07-20 14:45:25 -0500

    added login success and new activity

--- app/src/main/java/com/bcutlerywala/finalapp/MainActivity.java
+++ app/src/main/java/com/bcutlerywala/finalapp/MainActivity.java
@@ -8,9 +8,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public void tryLogin(View view) {
+//ello mate
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.bcutlerywala.sharedpreferences", Context.MODE_PRIVATE);
 
         EditText usernameEditText = (EditText) findViewById(R.id.emailText);
         EditText passwordEditText = (EditText) findViewById(R.id.passwordText);
@@ -18,13 +21,29 @@ public class MainActivity extends AppCompatActivity {
         String username = usernameEditText.getText().toString();
         String passwordEntered = passwordEditText.getText().toString();
 
+        String passwordStored = sharedPreferences.getString(username, "");
+
         Log.i("Info", "Username is:" + username);
         Log.i("Info", "Password entered is:" + passwordEntered);
+        Log.i("Info", "Password stored is:" + passwordStored);
+
+        if (passwordStored.equals(passwordEntered)) {
+            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            goToManage(view);
+        }
     }
+
+
+
 
     public void goToRegister(View view) {
         //Create and Intent in order ot launch the Register Activity
         Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intent);
+    }
+    public void goToManage(View view) {
+        //Create and Intent in order ot launch the Manage Activity
+        Intent intent = new Intent(getApplicationContext(), ManageActivity.class);
         startActivity(intent);
     }
     @Override
